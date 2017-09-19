fcf07bd34b4d5f344f94666d1b3ae321711dc0be
Zach Gibb <zachgibb@gmail.com>
2017-09-19 00:02:53 -0400

    [ADD] Rough out styling for basic sections of landing

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import { Alert, StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 import Logo from './components/Logo';
 import RoundedButton from './components/RoundedButton';
 
@@ -10,16 +10,25 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Logo />
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <RoundedButton
-          color="blue"
-          onPress={this.handlePress}
-        >
-          Create An Account
-        </RoundedButton>
+        <View style={styles.contentContainer}>
+          <Logo />
+          <Text style={styles.header}>Hoard</Text>
+          <Text style={styles.subtitle}>Digital currency for everyone</Text>
+        </View>
+        <View style={styles.actionsContainer}>
+          <RoundedButton
+            color="white"
+            onPress={this.handlePress}
+          >
+            Create An Account
+          </RoundedButton>
+          <View style={styles.signInContainer}>
+            <Text style={styles.signInPrompt}>Have an account?</Text>
+            <TouchableHighlight onPress={this.handlePress}>
+              <Text style={styles.signInButton}> Sign in!</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
       </View>
     );
   }
@@ -28,8 +37,40 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#0b2026',
   },
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 100,
+  },
+  header: {
+    fontSize: 70,
+    fontWeight: '100',
+    color: 'white',
+  },
+  subtitle: {
+    fontSize: 20,
+    fontWeight: '100',
+    fontStyle: 'italic',
+    color: 'white',
+  },
+  actionsContainer: {
+    marginTop: 'auto',
+    width: '100%',
+  },
+  signInContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    margin: 20,
+  },
+  signInPrompt: {
+    fontWeight: 'bold',
+    color: 'lightblue',
+  },
+  signInButton: {
+    fontWeight: 'bold',
+    color: 'white',
+  }
 });
