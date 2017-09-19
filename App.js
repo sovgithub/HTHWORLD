ab4acf28c3b7d414d1f8e2dc095bac1627e3a7e8
Zach Gibb <zachgibb@gmail.com>
2017-09-18 21:05:58 -0400

    [NEW] Add logo to home screen

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 // @flow
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Logo from './components/Logo';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Logo />
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
