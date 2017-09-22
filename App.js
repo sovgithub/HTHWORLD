16f9ca3ffe7e436879ff7d4d97b0322891c4d4c6
Zach Gibb <zachgibb@gmail.com>
2017-09-21 21:25:27 -0400

    [NEW] Use StackNavigator as entry point of app

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 // @flow
 import React from 'react';
 import Landing from './screens/Landing';
+import { StackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Landing />
-    );
-  }
-}
+const App = StackNavigator({
+  Landing:  { screen: Landing },
+}, {
+  headerMode: 'none',
+});
+
+export default App;
