9a91b3b60a1f07d335ec23673f154ffc15ac72c0
Zach Gibb <zachgibb@gmail.com>
2017-09-25 21:58:21 -0400

    [NEW] Setup basic auth reducer

--- store.duck.js
+++ store.duck.js
@@ -1,4 +1,6 @@
+// TYPES
 export type Store = {
+  accessToken?: string
 };
 
 export type Action = {
@@ -6,12 +8,44 @@ export type Action = {
   payload?: any
 };
 
+
+// CONSTANTS
+const LOGIN = 'LOGIN';
+const LOGOUT = 'LOGOUT';
+
+// REDUCER
 const initialState = {
+  accessToken: null
 };
 
-export default function baseReducer(state: State = initialState, action: Action) {
+export default function baseReducer(state: State = initialState, action: Action): State {
   switch(action.type) {
+  case LOGIN:
+    return {
+      ...state,
+      accessToken: action.payload
+    };
+  case LOGOUT:
+    return {
+      ...state,
+      accessToken: null
+    };
   default:
     return state;
   }
 }
+
+
+// ACTIONS
+function makeActionCreator(type: string, ...argNames: Array<string> ): func {
+  return function (...args: Array<any>): Action {
+    let action = { type }
+    argNames.forEach((arg, index) => {
+      action[argNames[index]] = args[index]
+    })
+    return action
+  }
+}
+
+export const login = makeActionCreator(LOGIN, 'payload');
+export const logout = makeActionCreator(LOGOUT);
