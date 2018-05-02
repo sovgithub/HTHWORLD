d0d454f7f521684ce93aaa844a5fededac86d1dd
Eric Gibb <ericgibb@gmail.com>
2018-05-02 13:50:53 -0400

    [NEW] added gradient support

--- android/app/src/main/java/com/hoard/MainApplication.java
+++ android/app/src/main/java/com/hoard/MainApplication.java
@@ -3,6 +3,7 @@ package com.hoard;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new SplashScreenReactPackage(),
             new VectorIconsPackage(),
             new SvgPackage(),
