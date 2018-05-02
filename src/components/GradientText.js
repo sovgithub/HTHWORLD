a02823b28216fe21626c6609420d1bebc8724a92
Eric Gibb <ericgibb@gmail.com>
2018-05-02 13:57:22 -0400

    [NEW] add gradient text, add gradient flippy card

---
+++ src/components/GradientText.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import { Text, MaskedViewIOS } from 'react-native';
+
+import LinearGradient from 'react-native-linear-gradient';
+
+export const GradientText = props => {
+  return (
+    <MaskedViewIOS maskElement={<Text {...props} />}>
+      <LinearGradient
+        start={{ x: 0, y: 1 }}
+        end={{ x: 1, y: 1 }}
+        colors={props.gradient}
+      >
+        <Text {...props} style={[props.style, { opacity: 0 }]} />
+      </LinearGradient>
+    </MaskedViewIOS>
+  );
+};
