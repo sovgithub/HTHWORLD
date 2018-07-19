29e0d949e508a02f07b5a532bf1222fa598ef25b
Eric Gibb <ericgibb@gmail.com>
2018-07-19 16:10:04 -0400

    [NEW] add urban airship package

--- android/app/src/main/java/com/hoard/MainApplication.java
+++ android/app/src/main/java/com/hoard/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
 import com.facebook.react.ReactApplication;
+import com.urbanairship.reactnative.ReactAirshipPackage;
 import org.reactnative.camera.RNCameraPackage;
 import io.fabric.sdk.android.Fabric;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactAirshipPackage(),
             new RNCameraPackage(),
             new LinearGradientPackage(),
             new RandomBytesPackage(),
