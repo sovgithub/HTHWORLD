3421af30736e76a0cb15e7ec22b96c8040adc2b5
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2018-10-06 06:48:05 -0400

    Update constants.js

--- src/screens/Settings/constants.js
+++ src/screens/Settings/constants.js
@@ -2,7 +2,7 @@ export const TRADING_PAIR_USD = 'USD';
 export const TRADING_PAIR_AUD = 'AUD';
 export const UPDATE_TRADING_PAIR = 'UPDATE_TRADING_PAIR';
 export const INITIALIZE_SETTINGS = 'INITIALIZE_SETTINGS';
-export const SETTINGS_STORAGE_KEY = '@hoard:settings';
+export const SETTINGS_STORAGE_KEY = '@HTHWORLD:settings';
 
 export const SUPPORTED_TRADING_PAIRS = [
   {
