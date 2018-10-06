264920ddafde828eb718ee6a91258c91e866f5bc
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2018-10-06 07:32:06 -0400

    Update OpenSource.js

--- src/screens/About/OpenSource.js
+++ src/screens/About/OpenSource.js
@@ -12,7 +12,7 @@ export default function OpenSource() {
             We would like to thank:
           </T.Heading>
           <T.Paragraph style={styles.type}>
-            Hoard, AltcoinCash, Devilking6105React, React Native, Ethers, 
+            Hoard, AltcoinCash, Devilking6105, React, React Native, Ethers, 
             React Navigation, Redux, React Redux, Redux Saga, Reselect, 
             Victory Native, Lottie React Native, Prop Types, 
             React Native Animatable, React Native Config, React Native
